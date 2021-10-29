0f634ec8b747085eac82ab01ae42d5653abeb2ef
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2021-10-28 22:29:53 -0300

    adicionando campo de texto em tela_cadastro_certificadora

--- lib/visao/certificadora/tela_cadastro_certificadora.dart
+++ lib/visao/certificadora/tela_cadastro_certificadora.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:organicos/controle/controle_cadastros.dart';
 import 'package:organicos/modelo/certificadora.dart';
+import 'package:organicos/visao/pontosvenda/tela_cadastro_produto.dart';
+import 'package:organicos/visao/widgets/mensagens.dart';
+import 'package:organicos/visao/widgets/textformfield.dart';
 
 class TelaCadastroCertificadora extends StatefulWidget{
    ControleCadastros<Certificadora> controle;
@@ -13,6 +16,19 @@ class TelaCadastroCertificadora extends StatefulWidget{
 }
 
 class _TelaCadastroCertificadoraState extends State<TelaCadastroCertificadora>{
+var _chaveFormulario = GlobalKey<FormState>();
+
+Future<void> Salvar(BuildContext context) async{
+  if(_chaveFormulario.currentState != null &&
+  _chaveFormulario.currentState!.validate()){
+    widget.controle.salvarObjetoCadastroEmEdicao().then((value){
+      if(widget.onSaved != null) widget.onSaved!();
+      Navigator.of(context).pop();
+    }).catchError((error){
+      mensagemConexao(context);
+    });
+  }
+}
 @override
 Widget build(BuildContext context) {
   return Scaffold(
@@ -20,6 +36,41 @@ Widget build(BuildContext context) {
       title: Text('Cadastro Certificadora'),
       centerTitle: true,
     ),
+    floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    floatingActionButton: FloatingActionButton.extended(
+      icon: Icon(Icons.check),
+      onPressed: (){
+        salvar(context);
+      }, label: Text('Salvar')),
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Form(
+              key: _chaveFormulario,
+              child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: decorationCampoTexto(
+                    hintText: "Nome", labelText: "Nome"),
+                    keyboardType: TextInputType.text,
+                    initialValue: 
+                    widget.controle.objetoCadastroEmEdicao?.nome,
+                  onSaved: (String? value){
+                    widget.controle.objetoCadastroEmEdicao?.nome = value;
+                  },
+                  validator: (value){
+                    if(value == null || value.trim().isEmpty){
+                      return "Este campo é obrigatório!";
+                    }
+                    return null;
+                  },
+                )
+              ],
+              ),
+            ),)
+        ],
+      ),
   );
 }
 }
