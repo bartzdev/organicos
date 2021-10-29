8288505b3c53242bf6fed8b9639b9046273f8cf0
Gabriel Locks <gabri.locks@gmail.com>
2021-10-28 21:34:41 -0300

    Inclusao do usuarioDao e GrupoUsuarioDAO

--- lib/dao/dao_factory.dart
+++ lib/dao/dao_factory.dart
@@ -2,11 +2,15 @@ import 'package:organicos/dao/cidade_dao.dart';
 import 'package:organicos/dao/dao.dart';
 import 'package:organicos/dao/estado_dao.dart';
 import 'package:organicos/dao/grupoprodutor_dao.dart';
+import 'package:organicos/dao/grupousuario_dao.dart';
 import 'package:organicos/dao/pontovenda_dao.dart';
+import 'package:organicos/dao/usuario_dao.dart';
 import 'package:organicos/modelo/cidade.dart';
 import 'package:organicos/modelo/estado.dart';
 import 'package:organicos/modelo/grupo_produtor.dart';
+import 'package:organicos/modelo/grupo_usuario.dart';
 import 'package:organicos/modelo/ponto_venda.dart';
+import 'package:organicos/modelo/usuario.dart';
 
 class DAOFactory<T> {
   DAO<T>? createDAO(T objectInstance) {
@@ -14,6 +18,8 @@ class DAOFactory<T> {
     if (objectInstance is Estado) return EstadoDAO() as DAO<T>;
     if (objectInstance is Cidade) return CidadeDAO() as DAO<T>;
     if (objectInstance is GrupoProdutor) return GrupoProdutorDAO() as DAO<T>;
+    if (objectInstance is Usuario) return UsuarioDAO() as DAO<T>;
+    if (objectInstance is GrupoUsuario) return GrupoUsuarioDAO() as DAO<T>;
     return null;
   }
 }
---
+++ lib/dao/grupousuario_dao.dart
@@ -0,0 +1,91 @@
+import 'package:organicos/dao/conexao.dart';
+import 'package:organicos/dao/dao.dart';
+import 'package:organicos/modelo/grupo_usuario.dart';
+
+class GrupoUsuarioDAO extends DAO<GrupoUsuario>{
+  @override
+  Future<void> gravar(GrupoUsuario grupoUsuario) async {
+    var conexao = await Conexao.getConexao();
+
+    await conexao.transaction((transacao) async {
+      if (grupoUsuario.id == null || grupoUsuario.id == 0) {
+        var resultadoInsert = await transacao.prepared('''insert into grupousuario 
+          (id, nome, registro_ativo) 
+          values 
+          (?, ?, ?)''', [
+          grupoUsuario.id,
+          grupoUsuario.nome,
+          grupoUsuario.ativo
+        ]);
+        grupoUsuario.id = resultadoInsert.insertId;
+      } else {
+        await transacao.prepared('''update grupousuario set
+          nome = ?, registro_ativo = ? where id = ?''', [
+          grupoUsuario.id,
+          grupoUsuario.nome,
+          grupoUsuario.ativo
+        ]);
+      }
+    });
+
+  }
+
+
+  @override
+  Future<GrupoUsuario> carregarDados(GrupoUsuario grupoUsuario, {Map<String, dynamic>? filtros})async {
+    var conexao = await Conexao.getConexao();
+    var resultadoConsulta = await conexao.prepared('''select 
+    id, nome, registro_ativo
+    from grupousuario 
+    where grupousuario.id = ?''', [grupoUsuario.id]);
+    await resultadoConsulta.forEach((linhaConsulta) {
+      grupoUsuario.id = linhaConsulta[0];
+      grupoUsuario.nome = linhaConsulta[1];
+      grupoUsuario.ativo = linhaConsulta[2];
+    });
+    return grupoUsuario;
+
+  }
+
+  @override
+  Future<void> excluir(GrupoUsuario grupoUsuario) async{
+    grupoUsuario.ativo = false;
+    await gravar(grupoUsuario);
+  }
+
+  @override
+  Future<List<GrupoUsuario>> listar({Map<String, dynamic>? filtros}) async{
+    List<GrupoUsuario> grupos = [];
+    var conexao = await Conexao.getConexao();
+    var resultadoConsulta = await conexao.prepared('''select 
+    id, nome
+    from grupousuario 
+    where grupousuario.registro_ativo = 1
+    order by lower(p.nome)''', []);
+    await resultadoConsulta.forEach((linhaConsulta) {
+      var grupoUsuario = GrupoUsuario();
+      grupoUsuario.id = linhaConsulta[0];
+      grupoUsuario.nome = linhaConsulta[1];
+      grupos.add(grupoUsuario);
+    });
+    return grupos;
+  }
+
+  @override
+  Future<List<GrupoUsuario>> pesquisar({Map<String, dynamic>? filtros}) async{
+    String filtro = filtros != null && filtros.containsKey('filtro') ? filtros['filtro'] : '';
+    List<GrupoUsuario> grupos = [];
+    var conexao = await Conexao.getConexao();
+    var resultadoConsulta = await conexao.prepared('''select 
+    id, nome
+    from grupousuario where grupousuario.registro_ativo = 1 and lower(p.nome) like ?
+    order by lower(p.nome)''', ['%${filtro.toLowerCase()}%']);
+    await resultadoConsulta.forEach((linhaConsulta) {
+      var grupoUsuario = GrupoUsuario();
+      grupoUsuario.id = linhaConsulta[0];
+      grupoUsuario.nome = linhaConsulta[1];
+      grupos.add(grupoUsuario);
+    });
+    return grupos;
+  }
+  }
