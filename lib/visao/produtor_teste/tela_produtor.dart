04cbbbc296787f2773efad85a554ff7b4915c233
DESKTOP-QU96IBT\IFPR <italo.rgrego@gmail.com>
2021-12-02 22:30:52 -0300

    Primeira versão da tela de Produtor

--- lib/visao/produtor_teste/tela_produtor.dart
+++ lib/visao/produtor_teste/tela_produtor.dart
@@ -1,169 +1,287 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:organicos/controle/controle_cadastros.dart';
-import 'package:organicos/modelo/cidade.dart';
-import 'package:organicos/modelo/grupo_produtor.dart';
-import 'package:organicos/visao/cidades/tela_pesquisa_cidades.dart';
-import 'package:organicos/visao/styles/styles.dart';
-import 'package:organicos/visao/widgets/mensagens.dart';
-import 'package:organicos/visao/widgets/textformfield.dart';
-import 'package:brasil_fields/brasil_fields.dart';
+import 'package:organicos/visao/login/loginControle.dart';
 
 class TelaProdutor extends StatefulWidget {
+  const TelaProdutor({Key? key}) : super(key: key);
+
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw TelaProdutorState();
-  }
-  
+  _TelaProdutorState createState() => _TelaProdutorState();
 }
 
-class TelaProdutorState extends State<TelaProdutor> {
-  var _chaveFormulario = GlobalKey<FormState>();
-
+class _TelaProdutorState extends State<TelaProdutor> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Cadastro de Produtor'),
-        centerTitle: true,
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton.extended(
-          icon: Icon(Icons.check), onPressed: () {}, label: Text('Salvar')),
-      body: ListView(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Form(
-                key: _chaveFormulario,
-                child: Column(
-                  children: <Widget>[
-                    TextFormField(
-                        decoration: decorationCampoTexto(
-                            hintText: "Nome", labelText: "Nome"),
-                        keyboardType: TextInputType.text,
-                        validator: (value) {
-                          if (value == null || value.trim().isEmpty) {
-                            return "Este campo é obrigatório!";
-                          }
-                          return null;
-                        }),
-                    espacoEntreCampos,
-                    TextFormField(
-                        decoration: decorationCampoTexto(
-                            hintText: "Cnpj", labelText: "Cnpj"),
-                        keyboardType: TextInputType.number,
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly,
-                          CnpjInputFormatter()
-                        ],
-                        validator: (value) {
-                          if (value == null || value.trim().isEmpty) {
-                            return "Este campo é obrigatório!";
-                          } else if (UtilBrasilFields.isCNPJValido(value) ==
-                              false) {
-                            return "Insira um cnpj valido";
-                          }
-                          return null;
-                        }),
-                    espacoEntreCampos,
-                    TextFormField(
-                        decoration: decorationCampoTexto(
-                            hintText: "Inscrição Estadual",
-                            labelText: "Inscrição Estadual"),
-                        keyboardType: TextInputType.text,
-                        inputFormatters: [LengthLimitingTextInputFormatter(20)],
-                        validator: (value) {
-                          if (value == null || value.trim().isEmpty) {
-                            return "Este campo é obrigatório!";
-                          }
-                          return null;
-                        }),
-                    espacoEntreCampos,
-                    TextFormField(
-                        decoration: decorationCampoTexto(
-                            hintText: "Endereço", labelText: "Endereço"),
-                        keyboardType: TextInputType.text,
-                        validator: (value) {
-                          if (value == null || value.trim().isEmpty) {
-                            return "Este campo é obrigatório!";
-                          }
-                          return null;
-                        }),
-                    espacoEntreCampos,
-                    TextFormField(
-                        decoration: decorationCampoTexto(
-                            hintText: "Numero", labelText: "Numero"),
-                        keyboardType: TextInputType.number,
-                        onSaved: (String? value) {
-                          print("Aqui");
-                          ;
-                        },
-                        validator: (value) {
-                          if (value == null || value.trim().isEmpty) {
-                            return "Este campo é obrigatório!";
-                          }
-                          return null;
-                        }),
-                    espacoEntreCampos,
-                    TextFormField(
-                        decoration: decorationCampoTexto(
-                            hintText: "Bairro", labelText: "Bairro"),
-                        keyboardType: TextInputType.text,
-                        validator: (value) {
-                          if (value == null || value.trim().isEmpty) {
-                            return "Este campo é obrigatório!";
-                          }
-                          return null;
-                        }),
-                    espacoEntreCampos,
-                    GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => TelaPesquisaCidades(
-                                        onItemSelected: (Cidade cidade) {
-                                          Navigator.of(context).pop();
-                                          setState(() {
-                                            print("Outro state");
-                                          });
-                                        },
-                                      )));
-                        },
-                        child: AbsorbPointer(
-                            child: TextFormField(
-                                decoration: decorationCampoTexto(
-                                    hintText: "Cidade", labelText: "Cidade"),
-                                keyboardType: TextInputType.text,
-                                validator: (value) {
-                                  if (value == null || value.trim().isEmpty) {
-                                    return "Este campo é obrigatório!";
-                                  }
-                                  return null;
-                                }))),
-                    CheckboxListTile(
-                      value: true,
-                      onChanged: (value) {
-                        setState(() {
-                          print('Mais um');
-                        });
-                      },
-                      title: Text('Distribui produtos'),
-                      controlAffinity: ListTileControlAffinity.leading,
-                      activeColor: Colors.blue,
-                    ),
-                    SizedBox(
-                      height: 60,
-                    )
-                  ],
-                )),
+      body: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.person),
+                  child: MediaQuery.of(context).size.width < 400
+                      ? null
+                      : Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            // Icon(Icons.person),
+                            // SizedBox(
+                            //   width: 10,
+                            // ),
+
+                            Expanded(
+                                child: Center(
+                                    child: Text(
+                              'Dados do proprietário',
+                              textAlign: TextAlign.center,
+                            ))),
+                          ],
+                        ),
+                ),
+                Tab(
+                  icon: Icon(Icons.account_tree),
+                  child: MediaQuery.of(context).size.width < 400
+                      ? null
+                      : Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            // Icon(Icons.person),
+                            // SizedBox(
+                            //   width: 10,
+                            // ),
+
+                            Expanded(
+                                child: Center(
+                                    child: Text(
+                              'Dados da propriedade',
+                              textAlign: TextAlign.center,
+                            ))),
+                          ],
+                        ),
+                ),
+                Tab(
+                  icon: Icon(Icons.description),
+                  child: MediaQuery.of(context).size.width < 400
+                      ? null
+                      : Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            // Icon(Icons.person),
+                            // SizedBox(
+                            //   width: 10,
+                            // ),
+
+                            Expanded(
+                                child: Center(
+                                    child: Text(
+                              'Dados da produção',
+                              textAlign: TextAlign.center,
+                            ))),
+                          ],
+                        ),
+                ),
+              ],
+            ),
+            title: Text('Tela Inicial'),
           ),
-        ],
+          body: TabBarView(
+            children: [
+              abaDadosPessoais(),
+              abaDadosDoProprietario(),
+              abaDadosDaPropriedade()
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  Widget abaDadosPessoais() {
+    return Scaffold(
+        body: Center(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: Wrap(
+            direction: Axis.horizontal,
+            alignment: WrapAlignment.start,
+            spacing: 30.0,
+            runSpacing: 30.0,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      onChanged: (text) {},
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          labelText: 'Nome', border: OutlineInputBorder()),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextField(
+                      onChanged: (text) {},
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          labelText: 'CPF/CNPJ', border: OutlineInputBorder()),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextField(
+                      onChanged: (text) {},
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          labelText: 'Cidade', border: OutlineInputBorder()),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextField(
+                      onChanged: (text) {},
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          labelText: 'Endereço', border: OutlineInputBorder()),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextField(
+                      onChanged: (text) {},
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          labelText: 'CEP', border: OutlineInputBorder()),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            onChanged: (text) {},
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                                labelText: 'Número',
+                                border: OutlineInputBorder()),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Expanded(
+                          child: TextField(
+                            onChanged: (text) {},
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                                labelText: 'Bairro',
+                                border: OutlineInputBorder()),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextField(
+                      onChanged: (text) {},
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          labelText: 'Telefone', border: OutlineInputBorder()),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.red[400],
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 50, vertical: 20),
+                                textStyle: const TextStyle(fontSize: 20)),
+                            onPressed: () {},
+                            child: Text('Cancelar')),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 50, vertical: 20),
+                                textStyle: const TextStyle(fontSize: 20)),
+                            onPressed: () {},
+                            child: Text('Continuar')),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+
+  Widget abaDadosDoProprietario() {
+    return Scaffold(
+        body: Center(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: Wrap(
+            direction: Axis.horizontal,
+            alignment: WrapAlignment.start,
+            spacing: 30.0,
+            runSpacing: 30.0,
+            children: [
+              Text(
+                  'Italo teste de alguma coisa colocada aqui dentro vamos ver'),
+              ElevatedButton(
+                onPressed: () {
+                  print('Fui clicado');
+                },
+                child: Text('Nome do meu botão'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+
+  Widget abaDadosDaPropriedade() {
+    return Scaffold(
+        body: Center(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: Wrap(
+            direction: Axis.horizontal,
+            alignment: WrapAlignment.start,
+            spacing: 30.0,
+            runSpacing: 30.0,
+            children: [
+              Text(
+                  'Italo teste de alguma coisa colocada aqui dentro vamos ver'),
+              ElevatedButton(
+                onPressed: () {
+                  print('Fui clicado');
+                },
+                child: Text('Nome do meu botão'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
   }
 }
