d3efd6f7f914a44ed12f196f4e6156a4833b9a5a
DESKTOP-GRAR8Q9\IFPR <rafael.ifpr.2019@gmail.com>
2021-12-06 19:44:35 -0300

    termino da tela sobre

--- lib/visao/sobre/sobre.dart
+++ lib/visao/sobre/sobre.dart
@@ -12,16 +12,21 @@ class Sobre extends StatelessWidget {
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         body: ListView(children: [
-          Padding(padding: EdgeInsets.all(30)),
-          Text(
-            '''Este aplicativo foi desenvolvido com o objetivo de aproximar os produtores da agricultura familiar dos seus consumidores.
-           O projeto foi desenvolvido pelos estudantes a, Gabriel Gaban de Lima, Gabriel Leopoldo Locks, Italo Rodrigues dos Santos, Karollyne de Paulo Marcola, Lucas  Wesolowski Medeiros, Rafael Shono
-           do Instituto Federal do Paraná campus Assis Chateaubriand, \na disciplina de programação para dispositivos móveis ministrada pelo professor doutor Rafael Luis Bartz. 
-          Atualmente o aplicativo faz parte de um projeto de extensão coordenado pela professora Sonia Mandotti, que faz o acompanhamento técnico dos produtores de Assis Chateaubriand 
-''',
-            textAlign: TextAlign.justify,
-            textWidthBasis: TextWidthBasis.longestLine,
-          )
+          Container(
+              width: MediaQuery.of(context).size.width,
+              height: 300,
+              color: Color(0xFF61b255),
+              child: Image.asset('assets/imagens/logoOrganico.jpeg')),
+          Padding(
+              padding: EdgeInsets.all(30),
+              child: Text(
+                'Este aplicativo foi desenvolvido com o objetivo de aproximar os produtores da agricultura familiar dos seus consumidores. ' +
+                    '\n   O projeto foi desenvolvido pelos estudantes a, Gabriel Gaban de Lima, Gabriel Leopoldo Locks, Italo Rodrigues dos Santos, Karollyne de Paulo Marcola, Lucas  Wesolowski Medeiros, Rafael Shono' +
+                    ' do Instituto Federal do Paraná campus Assis Chateaubriand, a disciplina de programação para dispositivos móveis ministrada pelo professor doutor Rafael Luis Bartz.' +
+                    '\n   Atualmente o aplicativo faz parte de um projeto de extensão coordenado pela professora Sonia Mandotti, que faz o acompanhamento técnico dos produtores de Assis Chateaubriand.',
+                textAlign: TextAlign.justify,
+                textWidthBasis: TextWidthBasis.longestLine,
+              ))
         ]));
   }
 }
